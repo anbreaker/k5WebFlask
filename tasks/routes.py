f243c4a1b60af64aeace7ec23d3ee14e1eb26784
anbreaker <antunez19@gmail.com>
2020-01-10 20:16:56 +0100

    Preparando para base de datos

--- tasks/routes.py
+++ tasks/routes.py
@@ -3,11 +3,15 @@ from flask import render_template, request, redirect, url_for
 from tasks.forms import TaskForm, ProccesTaskForm
 
 import csv
+import sqlite3
 from datetime import date
 
 DATOS = './data/tareas.txt'
 COPIA = './data/copia.txt'
+BASE_DATOS = './data/tasks.db'
+
 cabecera = ['title', 'description', 'date']
+
 
 @app.route("/")
 def index():
@@ -19,7 +23,7 @@ def index():
         registros.append(linea)
 
     fdatos.close()
-    return render_template("index.html", registros=registros) 
+    return render_template("index.html", registros=registros)
 
 
 @app.route("/newtask", methods=['GET', 'POST'])
@@ -45,6 +49,7 @@ def newTask():
     else:
         return render_template("task.html", form=form)
 
+
 @app.route("/processtask", methods=['GET', 'POST'])
 def proccesTask():
     form = ProccesTaskForm(request.form)
@@ -68,7 +73,8 @@ def proccesTask():
 
         if registroAct:
             if registroAct[2]:
-                fechaTarea = date(int(registroAct[2][:4]), int(registroAct[2][5:7]), int(registroAct[2][8:]))
+                fechaTarea = date(int(registroAct[2][:4]), int(
+                    registroAct[2][5:7]), int(registroAct[2][8:]))
             else:
                 fechaTarea = None
 
@@ -76,31 +82,31 @@ def proccesTask():
 
             if 'btnModificar' in request.values:
                 accion = 'M'
-            
+
             if 'btnBorrar' in request.values:
                 accion = 'B'
 
-
-            form = ProccesTaskForm(data={'ix': ix, 'title': registroAct[0], 'description': registroAct[1], 'date': fechaTarea, 'btn': accion})
+            form = ProccesTaskForm(
+                data={'ix': ix, 'title': registroAct[0], 'description': registroAct[1], 'date': fechaTarea, 'btn': accion})
 
         return render_template("processtask.html", form=form)
 
-
-    if form.validate():
-        print("Modificar el fichero")
-        '''
-        Crear fichero copia vacio en escritura
-        leer y copiar todos los registros desde tareas.txt a copia.txt hasta el anterior al que vamos a modificar
-        grabar el nuevo registro con los datos del formulario
-        leer y copiar el resto de los registros hasta el final
-        cerrar los dos ficheros
-        borrar tareas.txt
-        renombrar copia.txt a tareas.txt
-        '''
-
-        original = fopen(DATOS, 'r')
-        copia = fopen(COPIA, 'w')
-
+    if form.btn.data == 'B':
+        print('borrar Registro')
         return redirect(url_for('index'))
 
-    return render_template("processtask.html", form=form)
+    if form.btn.data == 'M':
+        if form.validate():
+            print("Modificar el fichero")
+            '''
+            Crear fichero copia vacio en escritura
+            leer y copiar todos los registros desde tareas.txt a copia.txt hasta el anterior al que vamos a modificar
+            grabar el nuevo registro con los datos del formulario
+            leer y copiar el resto de los registros hasta el final
+            cerrar los dos ficheros
+            borrar tareas.txt
+            renombrar copia.txt a tareas.txt
+            '''
+            return redirect(url_for('index'))
+
+        return render_template("processtask.html", form=form)
